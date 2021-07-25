42a9c0e67a1251af7d385ac244e43c9d1c9719d6
Drungdair <71859420+Drungdair@users.noreply.github.com>
2021-07-24 23:02:57 -0400

    Add files via upload

--- TSPlibReader.py
+++ TSPlibReader.py
@@ -2,6 +2,8 @@ from enum import Enum
 from decimal import *
 import os
 import math
+import Utilities
+import sys
 
 class Point:
     x = float
@@ -11,13 +13,6 @@ class Point:
         self.y = y
 
 #Tipos de instancias en TSPlib
-"""
-class Distance_type(Enum):
-    EUC_2D = "EUC_2D"
-    CEIL_2D = "CEIL_2D"
-    GEO = "GEO"
-    ATT = "ATT"
-"""
 Distance_type = ("EUC_2D","CEIL_2D","GEO","ATT")
 # Arreglo de estructuras que contiene las coordenadas, Tipo Point
 nodeptr = []
@@ -124,7 +119,6 @@ def round_distance (i, j):
     diferencia_x = Decimal(f"{nodeptr[i].x}") - Decimal(f"{nodeptr[j].x}")
     diferencia_y = Decimal(f"{nodeptr[i].y}") - Decimal(f"{nodeptr[j].y}")
     distancia = round(math.sqrt(pow(diferencia_x,2) + pow(diferencia_y,2)),2)
-    #print(f"Diferencia x = {diferencia_x}, Diferencia y = {diferencia_y}, Distancia = {distancia}, Redondeado = {round(distancia)}")
     return round(distancia)
 
 def ceil_distance (i, j):
@@ -140,22 +134,128 @@ def ceil_distance (i, j):
     distancia = round(math.sqrt(pow(diferencia_x,2) + pow(diferencia_y,2)),2)
     return math.ceil(distancia)
 
-#def geo_distance (i, j):
-    # Funcion: computa la distancia geometrica (redondeada al siguiente entero)
+def geo_distance (i, j):
+    # Funcion: computa la distancia geografica (redondeada al siguiente entero)
     #          entre dos nodos
     # Input: indices de dos nodos
     # Output: distancia entre dos nodos
     # Comentarios: adaptada desde el codigo de concorde. Para una definicion de
     #              como calcular esta distancia vea TSPLIB
 
+    deg = float
+    min = float
+    lati = float
+    latj = float
+    longi = float
+    longj = float
+    q1 = float
+    q2 = float
+    q3 = float
+    dd = int
+    x1 = nodeptr[i].x
+    x2 = nodeptr[j].x
+    y1 = nodeptr[i].y
+    y2 = nodeptr[j].y
+    RRR = 6378.388
 
+    deg = Utilities.dtrunc(x1)
+    min = float(Decimal(f"{x1}") - Decimal(f"{deg}"))
+    lati = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+    deg = Utilities.dtrunc(x2)
+    min = float(Decimal(f"{x2}") - Decimal(f"{deg}"))
+    latj = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+
+    deg = Utilities.dtrunc(y1)
+    min = float(Decimal(f"{y1}") - Decimal(f"{deg}"))
+    longi = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+    deg = Utilities.dtrunc(y2)
+    min = float(Decimal(f"{y2}") - Decimal(f"{deg}"))
+    longj = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+
+    q1 = math.cos(float(Decimal(f"{longi}") - Decimal(f"{longj}")))
+    q2 = math.cos(float(Decimal(f"{lati}") - Decimal(f"{latj}")))
+    q3 = math.cos(float(Decimal(f"{lati}") + Decimal(f"{latj}")))
+    dd = int(RRR * math.acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3))+ 1.0)
+    return dd
+
+def att_distance (i, j):
+    # Funcion: computa la distancia ATT (redondeada al siguiente entero) entre
+    #          dos nodos
+    # Input: indices de dos nodos
+    # Output: distancia entre dos nodos
+    # Comentarios: para un definicion de como calcular esta distancia vea TSPLIB
+
+    diferencia_x = Decimal(f"{nodeptr[i].x}") - Decimal(f"{nodeptr[j].x}")
+    diferencia_y = Decimal(f"{nodeptr[i].y}") - Decimal(f"{nodeptr[j].y}")
+    rij = math.sqrt(float(pow(diferencia_x,2) + pow(diferencia_y,2)) / 10.0)
+    tij = Utilities.dtrunc(rij)
+
+    if (tij < rij):
+        dij = int(tij + 1)
+    else:
+        dij = int(tij)
+    return dij
+
+def compute_distances():
+    # Funcion: computa las distancias entre todos los nodos
+    # Input: ninguno
+    # Ouput: guarda las distancias en una matriz en la variable distance
+    
+    global distance
+    matrix = [[]]
+    
+    for i in range(n):
+        for j in range(n):
+            if (distance_type == Distance_type[0]):
+                matrix.append([])
+                matrix[i].append(round_distance(i,j))
+            elif (distance_type == Distance_type[1]):
+                matrix.append([])
+                matrix[i].append(ceil_distance(i,j))
+            elif (distance_type == Distance_type[2]):
+                matrix.append([])
+                matrix[i].append(geo_distance(i,j))
+            elif (distance_type == Distance_type[3]):
+                matrix.append([])
+                matrix[i].append(att_distance(i,j))
+
+    distance = matrix.copy()
+
+def compute_nn_lists():
+    # Funcion: computa la lista de vecinos mas cercanos a cada nodo
+    # Input: ninguno
+    # Output: guarda los vecinos mas cercanos en una matriz en la variable nn_list
+
+    global nn_list
+    i = int
+    node = int
+    nn = int
+    distance_vector = []
+    help_vector = []
+    m_nnear = [[]]
+
+    nn = (n - 1)
+    for node in range(n): # Computar cnd-sets para todos los nodos
+        for i in range(n): # Copiar distancias desde nodo hacia otros nodos
+            if (node == 0):
+                distance_vector.append(distance[node][i])
+                help_vector.append(i)
+            else:
+                distance_vector[i] = distance[node][i]
+                help_vector[i] = i
+        distance_vector[node] = sys.maxsize # Ciudad no es el vecino mas cercano
+        Utilities.sort2(distance_vector, help_vector, 0, n-1)
+        for i in range(nn):
+            m_nnear.append([])
+            m_nnear[node].append(help_vector[i])
+    
+    nn_list = m_nnear.copy()
 
 TSPlibReader("Hola mundo.tsp")
+compute_distances()
+compute_nn_lists()
 
-print(round_distance(1,2))
-print(ceil_distance(1,2))
-
-"""
-for punto in nodeptr:
-    print(f"[{punto.x}, {punto.y}]")
-"""
+for i in range(n-1):
+    for j in range(n-1):
+        print(nn_list[i][j], end=", ")
+    print()
